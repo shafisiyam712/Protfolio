769f509f4075add57ccf2b42af4279ae0d573b5b
shafisiyam712 <shafisiyam674.com>
2025-06-22 03:17:51 +0600

    featured section completed

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,111 +2,237 @@ import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
   const frontendSkills = [
-    "React",
-    "Vue",
-    "TypeScript",
+    "HTML5",
     "TailwindCSS",
-    "Svelte",
+    "Bootstrap",
+    "JavaScript",
+    "React",
+    "Figma"
   ];
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  const backendSkills = ["Node.js", "Python", "Express.js", "MongoDB","Authentication", "MySQL"];
+  const othersSkills = ["C", "C++", "JAVA", "Analytical Skills", "Creative Writing","English"];
 
   return (
+    // <section
+    //   id="about"
+    //   className="min-h-screen flex items-center justify-center py-20"
+    // >
+    //   <RevealOnScroll>
+    //     <div className="max-w-3xl mx-auto px-4">
+    //       <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+    //         {" "}
+    //         About Me
+    //       </h2>
+
+    //       <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+    //         <p className="text-gray-300 mb-6">
+    //           Passionate developer with expertise in building scalable web
+    //           applications and creating innovative solutions.
+    //         </p>
+
+    //         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+    //           <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+    //             <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+    //             <div className="flex flex-wrap gap-2">
+    //               {frontendSkills.map((tech, key) => (
+    //                 <span
+    //                   key={key}
+    //                   className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+    //                                 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+    //                 "
+    //                 >
+    //                   {tech}
+    //                 </span>
+    //               ))}
+    //             </div>
+    //           </div>
+
+    //           <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+    //             <h3 className="text-xl font-bold mb-4">Backend</h3>
+    //             <div className="flex flex-wrap gap-2">
+    //               {backendSkills.map((tech, key) => (
+    //                 <span
+    //                   key={key}
+    //                   className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+    //                                 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+    //                 "
+    //                 >
+    //                   {tech}
+    //                 </span>
+    //               ))}
+    //             </div>
+    //           </div>
+    //           <div className="rounded-xl p-6 hover:-translate-y-1 transition-all items-center text-center">
+    //             <h3 className="text-xl font-bold mb-4"> Others</h3>
+    //             <div className="flex flex-wrap gap-2">
+    //               {othersSkills.map((tech, key) => (
+    //                 <span
+    //                   key={key}
+    //                   className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+    //                                 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+    //                 "
+    //                 >
+    //                   {tech}
+    //                 </span>
+    //               ))}
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+
+    //       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+    //         <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+    //           <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+    //           <ul className="list-disc list-inside text-gray-300 space-y-2">
+    //             <li>
+    //               <strong> B.S. in Computer Science </strong> - XYZ University
+    //               (2016-2020)
+    //             </li>
+    //             <li>
+    //               Relevant Coursework: Data Structures, Web Development, Cloud
+    //               Computing...
+    //             </li>
+    //           </ul>
+    //         </div>
+    //         <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+    //           <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+    //           <div className="space-y-4 text-gray-300">
+    //             <div>
+    //               <h4 className="font-semibold">
+    //                 {" "}
+    //                 Software Engineer at ABC Corp (2020 - Present){" "}
+    //               </h4>
+    //               <p>
+    //                 Developed and maintained microservices for cloud-based
+    //                 applications.
+    //               </p>
+    //             </div>
+
+    //             <div>
+    //               <h4 className="font-semibold">
+    //                 {" "}
+    //                 Intern at DEF Startups (2019){" "}
+    //               </h4>
+    //               <p>
+    //                 Assisted in building front-end components and integration
+    //                 REST APIs
+    //               </p>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </RevealOnScroll>
+    // </section>
     <section
-      id="about"
-      className="min-h-screen flex items-center justify-center py-20"
-    >
-      <RevealOnScroll>
-        <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
-            About Me
-          </h2>
+  id="about"
+  className="min-h-screen flex items-center justify-center py-20"
+>
+  <RevealOnScroll>
+    <div className="max-w-3xl mx-auto px-4">
+      <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+        About Me
+      </h2>
 
-          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-            <p className="text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
-            </p>
+      <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+        <p className="text-gray-300 mb-6">
+          I am Shafiuddin Seyam, a passionate BSc Engineering student in Information and Communication Technology (ICT) at Mawlana Bhashani Science and Technology University. I specialize in front-end development with a solid understanding of backend technologies and a growing interest in full-stack development. With a good knowledge of C, C++, Java, and Python, I approach challenges with a problem-solving mindset and a drive for continuous improvement.I enjoy building seamless, efficient applications and am eager to grow as a developer by exploring modern tools, frameworks, and development practices. My enthusiasm for full-stack development drives me to learn continuously and solve problems with innovative solutions.Here are my skills....
+        </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {frontendSkills.map((tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                    "
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
-
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* Frontend */}
+          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">Frontend</h3>
+            <div className="flex flex-wrap gap-2">
+              {frontendSkills.map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
-              <ul className="list-disc list-inside text-gray-300 space-y-2">
-                <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
-                </li>
-                <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
-                </li>
-              </ul>
+          {/* Backend */}
+          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">Backend</h3>
+            <div className="flex flex-wrap gap-2">
+              {backendSkills.map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
-              <div className="space-y-4 text-gray-300">
-                <div>
-                  <h4 className="font-semibold">
-                    {" "}
-                    Software Engineer at ABC Corp (2020 - Present){" "}
-                  </h4>
-                  <p>
-                    Developed and maintained microservices for cloud-based
-                    applications.
-                  </p>
-                </div>
+          </div>
 
-                <div>
-                  <h4 className="font-semibold">
-                    {" "}
-                    Intern at DEF Startups (2019){" "}
-                  </h4>
-                  <p>
-                    Assisted in building front-end components and integration
-                    REST APIs
-                  </p>
-                </div>
-              </div>
+          {/* Others (centered and full-width) */}
+          <div className="md:col-span-2 rounded-xl p-6 hover:-translate-y-1 transition-all text-center flex flex-col items-center">
+            <h3 className="text-xl font-bold mb-4">Others</h3>
+            <div className="flex flex-wrap justify-center gap-2">
+              {othersSkills.map((tech, key) => (
+                <span
+                  key={key}
+                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                >
+                  {tech}
+                </span>
+              ))}
             </div>
           </div>
         </div>
-      </RevealOnScroll>
-    </section>
+      </div>
+
+      {/* Education & Experience */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+        <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+          <h3 className="text-xl font-bold mb-4">🏫 Education</h3>
+          <ul className="list-disc list-inside text-gray-300 space-y-2">
+            <li>
+              <strong>B.S. in Information & Communication Technology</strong> - Mawlana Bashani Science & Technology University
+              (2022-2026)
+            </li>
+            <li>
+              Relevant Coursework: Data Structures, Web Development, Java
+              Programming...
+            </li>
+          </ul>
+        </div>
+        <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+          <h3 className="text-xl font-bold mb-4">💼 Work Experience</h3>
+          {/* <div className="space-y-4 text-gray-300">
+            <div>
+              <h4 className="font-semibold">
+                Software Engineer at ABC Corp (2020 - Present)
+              </h4>
+              <p>
+                Developed and maintained microservices for cloud-based
+                applications.
+              </p>
+            </div>
+
+            <div>
+              <h4 className="font-semibold">
+                Intern at DEF Startups (2019)
+              </h4>
+              <p>
+                Assisted in building front-end components and integrating REST
+                APIs.
+              </p>
+            </div>
+          </div> */}
+          Still none..but will be added soon
+        </div>
+      </div>
+    </div>
+  </RevealOnScroll>
+</section>
+
+    
   );
 };
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,13 +14,12 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold mb-2">Movie Portal Website</h3>
               <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
+                It is a responsive single-page application designed for movie lovers to explore, contribute to, and organize their favorite films. The platform supports Google authentication and delivers a seamless user experience across all devices. Users can browse community-recommended movies, add their own, and curate a personal favorites list.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                {["React", "Node.js", "TailwindCSS", "MongoDB"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -34,7 +33,7 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://my-movie-site-1a1ab.web.app/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -49,13 +48,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <h3 className="text-xl font-bold mb-2">Food Sharing Website</h3>
               <p className="text-gray-400 mb-4">
-                ML-powered data visualization platform with predictive analytics
-                and interactive reports.
+               Food Feasta enables users to donate and receive food in times of need through a seamless, secure, and user-friendly platform. The application supports Google authentication and provides a fully responsive experience across all devices, making food sharing efficient, accessible, and impactful.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                {["React", "Node.js", "TailwindCSS", "MongoDB","express.js","Tanstack query"].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -72,7 +70,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://my-food-web.web.app/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -88,13 +86,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+              <h3 className="text-xl font-bold mb-2">Gadget Heaven</h3>
               <p className="text-gray-400 mb-4">
-                Full-stack e-commerce with modern UI, secure payment
-                integration, and customizable product inventory.
+                Gadget Heaven is a Frontend based website where you can find your needed tec product. We have different categories of product with a beautiful user interface.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
+                {["React", "TailwindCSS", "DaisyUI"].map(
                   (tech) => (
                     <span
                       key={tech}
@@ -113,7 +110,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://remarkable-croquembouche-06b9d2.netlify.app/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -129,13 +126,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <h3 className="text-xl font-bold mb-2">Dream-11 Cricket</h3>
               <p className="text-gray-400 mb-4">
-                Scalable chat platform supporting real-time messaging, presence,
-                and group chat features.
+               This is a simple react web based project(Frontend).Its platform where you can build your team. Players all over the world are here with their stat you just have to chose them using your cricket sense and build your team.For the transaction we give you coin so that you can bye players.I am very excited to launch this project!!
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                {["TailwindCSS", "React", "DaisyUI"].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -152,7 +148,44 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center ">
                 <a
-                  href="#"
+                  href="https://dear-suggestion.surge.sh/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+                 <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Simple Chess Game</h3>
+              <p className="text-gray-400 mb-4">
+                This is something I wanted to try.Build a simple chess game using the html css and js.Here I polish my js knowledge more for the game logic.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML5", "CSS", "JavaScript"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="https://shafisiyam712.github.io/Chess-Game/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
